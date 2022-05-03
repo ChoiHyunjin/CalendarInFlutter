eeed21b5a8b8c38619131a2d3825814d9cff591c
choi hyunjin <hyunjin@munch-factory.com>
2022-05-04 00:47:09 +0900

    [db] 특정 월 데이터 호출 함수 추가

--- lib/src/utils/preference.dart
+++ lib/src/utils/preference.dart
@@ -4,6 +4,7 @@ import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
 const scheduleTable = 'schedules';
+
 class Preference {
   static Preference shared = Preference._internal();
 
@@ -16,7 +17,8 @@ class Preference {
 
   Preference._internal() {
     getDatabasesPath().then((res) async {
-      db = await openDatabase(join(await getDatabasesPath(), scheduleTable, '.db'),
+      db = await openDatabase(
+          join(await getDatabasesPath(), scheduleTable, '.db'),
           onCreate: (db, version) {
         return db.execute(
             "CREATE TABLE schedules(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, userId TEXT, title TEXT, content TEXT, startDate INTEGER, startHour INTEGER, startMinute INTEGER, endDate INTEGER, endHour INTEGER, endMinute INTEGER, people TEXT)");
@@ -35,15 +37,31 @@ class Preference {
 
   Future<List<Schedule>> load() async {
     final List<Map<String, dynamic>> maps =
-    await db.query(scheduleTable, where: "userId = ?", whereArgs: [id]);
+        await db.query(scheduleTable, where: "userId = ?", whereArgs: [id]);
     debugPrint('load: $id, $maps');
     return List.generate(maps.length, (index) => Schedule.from(maps[index]));
   }
 
   Future<List<Schedule>> loadAt(int date) async {
-    final List<Map<String, dynamic>> maps =
-    await db.query(scheduleTable, where: "userId = ? AND startDate <= ? AND endDate >= ?", whereArgs: [id, date, date]);
+    final List<Map<String, dynamic>> maps = await db.query(scheduleTable,
+        where: "userId = ? AND startDate <= ? AND endDate >= ?",
+        whereArgs: [id, date, date]);
     debugPrint('loadAt: $id, $maps');
+    return List.generate(maps.length, (index) => Schedule.from(maps[index]));
+  }
+
+  Future<List<Schedule>> loadOn(int year, int month) async {
+    final first = DateTime.utc(year, month);
+    final last =
+        DateTime.utc(year, month + 1).subtract(const Duration(days: 1));
+    final firstDate = Schedule.mergeDate(first.year, first.month, first.day);
+    final lastDate = Schedule.mergeDate(last.year, last.month, last.day);
+    debugPrint('loadOn input: $firstDate, $lastDate');
+    final List<Map<String, dynamic>> maps = await db.query(scheduleTable,
+        where:
+            "userId = ? AND ((startDate >= ? AND startDate <= ?) OR (endDate >= ? AND endDate <= ?))",
+        whereArgs: [id, firstDate, lastDate, firstDate, lastDate]);
+    debugPrint('loadOn: $id, $maps');
     return List.generate(maps.length, (index) => Schedule.from(maps[index]));
   }
 

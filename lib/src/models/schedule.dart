42dc90c84ac74c02605e46e2504a739da8764b3b
choi hyunjin <hyunjin@munch-factory.com>
2022-05-04 00:50:15 +0900

    [스케줄] from 함수 수정

--- lib/src/models/schedule.dart
+++ lib/src/models/schedule.dart
@@ -2,24 +2,34 @@ import 'package:calendar/src/utils/preference.dart';
 
 class Schedule {
   static Schedule from(Map<String, dynamic> input) {
+    final start = input['startDate'].toString();
+    final end = input['endDate'].toString();
     return Schedule(
         id: input['id'],
         title: input['title'],
         content: input['content'],
-        startDate: DateTime(input['startYear'], input['startMonth'],
-            input['startDay'], input['startHour'], input['startMinute']),
-        endDate: DateTime(input['endYear'], input['endMonth'], input['endDay'],
-            input['endHour'], input['endMinute']),
+        startDate: DateTime(
+            int.parse(start.substring(0, 4)),
+            int.parse(start.substring(4, 6)),
+            int.parse(start.substring(6, 8)),
+            input['startHour'],
+            input['startMinute']),
+        endDate: DateTime(
+            int.parse(end.substring(0, 4)),
+            int.parse(end.substring(4, 6)),
+            int.parse(end.substring(6, 8)),
+            input['endHour'],
+            input['endMinute']),
         people: input['people']);
   }
 
-  static int mergeDate(int year, int month, int day){
+  static int mergeDate(int year, int month, int day) {
     var res = year.toString();
-    if(month.toString().length < 2){
+    if (month.toString().length < 2) {
       res += '0';
     }
     res += month.toString();
-    if(day.toString().length < 2){
+    if (day.toString().length < 2) {
       res += '0';
     }
     res += day.toString();
@@ -78,7 +88,8 @@ class Schedule {
       'userId': userId,
       'title': title,
       'content': content,
-      'startDate': Schedule.mergeDate(startDate.year, startDate.month, startDate.day),
+      'startDate':
+          Schedule.mergeDate(startDate.year, startDate.month, startDate.day),
       'startHour': startDate.hour,
       'startMinute': startDate.minute,
       'endDate': Schedule.mergeDate(endDate.year, endDate.month, endDate.day),
